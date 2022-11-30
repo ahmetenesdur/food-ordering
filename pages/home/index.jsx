c618d4374993d4573eb767c07da61aff2ed30efb
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-11-30 22:33:20 +0300

    Menu Wrapper Component

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Campaigns from '../../components/ui/Campaigns'
-import Carousel from '../../components/ui/Carousel'
+import Campaigns from '../../components/Campaigns'
+import Carousel from '../../components/Carousel'
+import MenuWrapper from '../../components/product/MenuWrapper'
 
 function Home() {
     return (
         <div>
             <Carousel />
             <Campaigns />
+            <MenuWrapper />
         </div>
     )
 }
