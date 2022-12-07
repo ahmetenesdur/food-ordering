4b285ded366fa06fa12fb42f6993e46c35b57096
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-12-07 22:00:57 +0300

    Cart Page Table

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -4,20 +4,20 @@ import Title from '../../components/ui/Title'
 function Cart() {
     return (
         <div className="min-h-[calc(100vh_-_433px)]">
-            <div className="flex justify-between items-center">
-                <div className="min-h-[calc(100vh_-_433px)] flex items-center flex-1">
-                    <table>
-                        <thead>
+            <div className="flex justify-between items-center md:flex-row flex-col">
+                <div className="md:min-h-[calc(100vh_-_433px)] flex items-center flex-1 p-10 overflow-x-auto w-full">
+                    <table className="w-full text-sm text-center text-gray-500 min-w-[1000px]">
+                        <thead className="text-xs text-gray-400 uppercase bg-gray-700">
                             <tr>
-                                <th>PRODUCT</th>
-                                <th>EXTRAS</th>
-                                <th>PRICE</th>
-                                <th>QUANTITY</th>
+                                <th scope="col" className="py-3 px-6">PRODUCT</th>
+                                <th scope="col" className="py-3 px-6">EXTRAS</th>
+                                <th scope="col" className="py-3 px-6">PRICE</th>
+                                <th scope="col" className="py-3 px-6">QUANTITY</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>
+                            <tr className="transition-all bg-secondary border-gray-700 hover:bg-primary">
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
                                     <Image
                                         src="/images/f1.png"
                                         alt=""
@@ -27,17 +27,17 @@ function Cart() {
                                     />
                                     <span>Good Pizza</span>
                                 </td>
-                                <td>
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
                                     <span>mayonnaise, hot sauce, ketchup,</span>
                                 </td>
-                                <td>$20</td>
-                                <td>1</td>
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">$20</td>
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">1</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
 
-                <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12">
+                <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12 md:w-auto w-full md:text-start !text-center">
                     <Title addClass="text-[40px]">CART TOTAL</Title>
 
                     <div className="mt-6">
@@ -46,7 +46,11 @@ function Cart() {
                         <b>Total: </b>$20
                     </div>
 
-                    <button className="btn-primary mt-4">CHECKOUT NOW!</button>
+                    <div>
+                        <button className="btn-primary mt-4 md:w-auto w-52">
+                            CHECKOUT NOW!
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
