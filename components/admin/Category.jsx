dde871e208586b53771e6ba41d035f8354f18606
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-12-10 19:22:44 +0300

    Admin Page Category Component

--- components/admin/Category.jsx
+++ components/admin/Category.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import { useState } from "react";
+import Title from '../ui/Title'
+import Input from './../form/Input';
 
 function Category() {
+    const [inputText, setInputText] = useState("");
+    const [categories, setCategories] = useState(["pizza"]);
+
     return (
-        <div>Category</div>
+        <div className="lg:p-8 flex-1 lg:mt-0 mt-5">
+            <Title addClass="text-[40px]">Category</Title>
+            <div className="mt-5">
+                <div className="flex gap-4 flex-1 items-center">
+                    <Input
+                        placeholder="Add a new Category..."
+                        onChange={(e) => setInputText(e.target.value)}
+                        value={inputText}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                if (inputText) {
+                                    setCategories([...categories, inputText]);
+                                    setInputText("");
+                                }
+                            }
+                        }}
+                    />
+                    <button
+                        className="btn-primary"
+                        onClick={() => {
+                            if (inputText) {
+                                setCategories([...categories, inputText]);
+                                setInputText("");
+                            }
+                        }}
+                    >
+                        Add
+                    </button>
+                </div>
+                <div className="mt-10">
+                    {categories.map((category, index) => (
+                        <div className="flex justify-between mt-4" key={index}>
+                            <b className="text-xl">{category}</b>
+                            <button
+                                className="btn-primary !bg-danger hover:!bg-dangerDark"
+                                onClick={() =>
+                                    setCategories(categories.filter((cat) => cat !== category))
+                                }
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
 
