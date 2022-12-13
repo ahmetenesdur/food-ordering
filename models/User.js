d1433c500d1f3b81f59d7a37d85d30acaef549bb
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-12-13 19:41:45 +0300

    Creating User Model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        phoneNumber: {
+            type: String,
+        },
+        address: {
+            type: String,
+        },
+        job: {
+            type: String,
+        },
+        bio: {
+            type: String,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        confirmPassword: {
+            type: String,
+            required: true,
+        },
+        emailVerified: {
+            type: string,
+            default: null,
+        },
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model.User || mongoose.model("User", UserSchema);
