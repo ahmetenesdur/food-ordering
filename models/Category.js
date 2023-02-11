561619da6cbce68ceb72eecb3e39b5ab0e0e6302
Enes <70810472+ahmetenesdur@users.noreply.github.com>
2023-02-12 00:18:42 +0300

    Order Model and Api

--- models/Category.js
+++ models/Category.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const CategorySchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-            maxlength: 60,
-        },
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 60,
     },
-    { timestamps: true }
+  },
+  { timestamps: true }
 );
 
-export default mongoose.models.Category || mongoose.model("Category", CategorySchema);
+export default mongoose.models.Category ||
+  mongoose.model("Category", CategorySchema);
--- models/User.js
+++ models/User.js
@@ -1,42 +1,42 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
-    {
-        fullName: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        phoneNumber: {
-            type: String,
-        },
-        address: {
-            type: String,
-        },
-        job: {
-            type: String,
-        },
-        bio: {
-            type: String,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        confirmPassword: {
-            type: String,
-            required: true,
-        },
-        emailVerified: {
-            type: String,
-            default: null,
-        },
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    job: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+    },
+    emailVerified: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
 );
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
