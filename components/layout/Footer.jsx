ce49792f8a84677b126bce290c665d521963f7ae
Enes <70810472+ahmetenesdur@users.noreply.github.com>
2023-02-13 20:44:16 +0300

    Footer Data Overlay

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import Title from './../ui/Title';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Title from "../ui/Title";
 
-function Footer() {
+const Footer = () => {
+  const [footer, setFooter] = useState([]);
+
+  useEffect(() => {
+    const getFooter = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/footer`
+        );
+        setFooter(res.data[0]);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getFooter();
+  }, []);
+
+  console.log(footer);
   return (
     <div className="bg-secondary text-white">
       <div className="container mx-auto pt-16 pb-6">
@@ -9,92 +27,59 @@ function Footer() {
           <div className="md:flex-1">
             <Title addClass="text-[30px]">Contact Us</Title>
             <div className="flex flex-col gap-y-2 mt-3">
-              <a
-                href="https://www.google.com/maps?ll=49.999029,36.248372&z=15&t=m&hl=tr&gl=TR&mapclient=embed&cid=325524714523145142"
-                className="hover:text-primary"
-              >
-                <div>
-                  <i className="fa-sharp fa-solid fa-location-dot"></i>
-                  <span className="inline-block ml-2">Location</span>
-                </div>
+              <a href={footer?.location} target="_blank" rel="noreferrer">
+                <i className="fa-sharp fa-solid fa-location-dot"></i>
+                <span className="inline-block ml-2">Location</span>
               </a>
-              <a
-                href="tel:PHONE_NUM"
-                className="hover:text-primary"
-              >
-                <div>
-                  <i className="fa fa-phone"></i>
-                  <span className="inline-block ml-2">Call +01 1234567890</span>
-                </div>
+              <a href={`tel:${footer?.phoneNumber}`}>
+                <i className="fa fa-phone"></i>
+                <span className="inline-block ml-2">
+                  Call +90 {footer?.phoneNumber}
+                </span>
               </a>
-              <a
-                href="mailto:demo@gmail.com"
-                className="hover:text-primary"
-              >
-                <div>
-                  <i className="fa fa-envelope"></i>
-                  <span className="inline-block ml-2">demo@gmail.com</span>
-                </div>
+              <a href={`mailto:${footer?.email}`}>
+                <i className="fa fa-envelope"></i>
+                <span className="inline-block ml-2">{footer?.email}</span>
               </a>
             </div>
           </div>
           <div className="md:flex-1">
             <Title addClass="text-[38px]">Feane</Title>
-            <p className="mt-3">
-              Necessary, making this the first true generator on the Internet.
-              It uses a dictionary of over 200 Latin words, combined with
-            </p>
+            <p className="mt-3">{footer?.desc}</p>
             <div className="flex items-center justify-center mt-5 gap-x-2">
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary"
-              >
-                <i className="fa-brands fa-facebook-f"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary"
-              >
-                <i className="fa-brands fa-twitter"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary"
-              >
-                <i className="fa-brands fa-linkedin-in"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary"
-              >
-                <i className="fa-brands fa-instagram"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-primary"
-              >
-                <i className="fa-brands fa-pinterest"></i>
-              </a>
+              {footer?.socialMedia?.map((item) => (
+                <a
+                  href={item?.link}
+                  className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full hover:text-white hover:bg-primary transition-all"
+                  key={item._id}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className={item.icon}></i>
+                </a>
+              ))}
             </div>
           </div>
           <div className="md:flex-1">
             <Title addClass="text-[30px]">Opening Hours</Title>
             <div className="flex flex-col gap-y-2 mt-3">
               <div>
-                <span className="inline-block ml-2">Everyday</span>
+                <span className="inline-block ml-2">
+                  {footer?.openingHours?.day}
+                </span>
               </div>
               <div>
-                <span className="inline-block ml-2">10.00 Am -10.00 Pm</span>
+                <span className="inline-block ml-2">
+                  {footer?.openingHours?.hour}
+                </span>
               </div>
             </div>
           </div>
         </div>
-        <p className="text-center mt-10">
-          © 2022 Feane. All Rights Reserved.
-        </p>
+        <p className="text-center mt-10">© 2023 Feane. All Rights Reserved.</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
